e109dea47978cd0f1d37dff23be6fb3aa633b98c
Jed Palmater <jed@palmater.ca>
2018-01-17 11:58:13 -0400

    login git probs

---
+++ loginfunction.php
@@ -0,0 +1,23 @@
+<?php 
+	include(connect.php);
+	$studentID = $_POST("studentID");
+	$result = mysqli_query($con, "CALL SP_getUsername($studentID)")
+	//if there is a user with that name check, continue and get their password
+	if ($result > 0){
+		 $pwArray = mysqli_fetch_array($con, "CALL SP_fetchStudentPassword($studentID)")
+		 while ($dRow = mysqli_fetch_array($pwArray)) {
+             $pw = ($pwArray["password"]);
+         }
+         $passwordtrue = password_verify($_POST("pw"), $dbpasswd);
+         if ($passwordtrue){
+         	$_SESSION['studentID'] = $studentID;
+         	$_SESSION['loggedin'] = $true;
+			header('location:userhome.php');
+		}
+		else {
+			//incorrect password error here
+		}
+	else {
+		//code for no user found error 
+	}		
+?>
