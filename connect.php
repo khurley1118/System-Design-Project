51f0a3eab843900fa2ac03fa34350ad8d05671d5
Jed Palmater <jed@palmater.ca>
2018-01-24 10:38:34 -0400

    changed connect.php for server configuration

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 //these are defined as constants
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'nbcctupro');
 define('DB_NAME', 'tupro');
 global $con;
 	$con = mysqli_connect(DB_HOST,DB_USER,DB_PASS, DB_NAME);
