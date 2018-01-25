c922d03f33e44763f54e9e8f68cad05bd7138e91
Jed Palmater <jed@palmater.ca>
2018-01-25 13:12:20 -0400

    fixed css link to style.css

--- facultyLogin.php
+++ facultyLogin.php
@@ -11,7 +11,6 @@
     <link rel="stylesheet" type="text/css" href="css/defaultLogin.css">
     <link rel="stylesheet" type="text/css" href="css/LogStyle.css">
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
     <link href="indexCSS.css" rel="stylesheet" type="text/css"/>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script>
