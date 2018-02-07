5b6a1378abf41034727aa1c4086dcba6dfdb7614
Jed Palmater <jed@palmater.ca>
2018-02-07 10:08:49 -0400

    removed diagnostic alert from adminLogin.php

--- adminLogin.php
+++ adminLogin.php
@@ -37,7 +37,7 @@ include("connect.php");
 		            data: {login: id,password: pw,type: "admin"},
 		            cache: false,
 		            success: function(data) {
-									alert(data);
+									//alert(data);
 									$("#formAlert").html(data);
 									if ((JSON.parse(data)) == "Logged In") {
 											window.location.replace('AdminPage.php');
