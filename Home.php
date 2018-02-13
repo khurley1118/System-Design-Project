865e745fd88082ad65e7959f048a7897f2218bbf
MarkEPat <markedward93@hotmail.com>
2018-02-13 10:14:39 -0400

    Initial push with base accordion

--- Home.php
+++ Home.php
@@ -1,8 +1,14 @@
 <!DOCTYPE html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <?php
 include('connect.php');
 include('Header.php');
 include('Footer.php');
+
+//if user id is not set then user is not logged in and is redirected to index.
+if (!isset($_SESSION['userID']) || $_SESSION['userID'] == ""){
+	header("location: index.php");
+}
 // set local variables for use from session variables.
 $id = $_SESSION['userID'];
 $userType = $_SESSION['userType'];
@@ -24,20 +30,44 @@ $user = $_SESSION['CurrentUser'];
       <div id="pageContent">
           <div id="flexBox">
             <div id="sideBar">
-                <div id="titleBar">
-                    <div id="titleText">
-                    <h3>Toolbar</h3>
-                    </div>
-                </div>
+
                 <div id="sideBarContent">
-                    <table>
-                        <tr>Item 1</tr><br>
-                        <tr>Item 2</tr><br>
-                        <tr>Item 3</tr><br>
-                        <tr>Item 4</tr><br>
-                        <tr>Item 5</tr><br>
-                        <tr>Item 6</tr><br>
-                    </table>
+									<div class="container">
+<h2>Accordion Example</h2>
+<p><strong>Note:</strong> The <strong>data-parent</strong> attribute makes sure that all collapsible elements under the specified parent will be closed when one of the collapsible item is shown.</p>
+<div class="panel-group" id="accordion">
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<a data-toggle="collapse" data-parent="#accordion" href="#collapse1">Group 1</a>
+			</h4>
+		</div>
+		<div id="collapse1" class="panel-collapse collapse in">
+			<div class="panel-body">Lorem</div>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<a data-toggle="collapse" data-parent="#accordion" href="#collapse2">Group 2</a>
+			</h4>
+		</div>
+		<div id="collapse2" class="panel-collapse collapse">
+			<div class="panel-body">Lorem</div>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<a data-toggle="collapse" data-parent="#accordion" href="#collapse3">Group 3</a>
+			</h4>
+		</div>
+		<div id="collapse3" class="panel-collapse collapse">
+			<div class="panel-body">Lorem</div>
+		</div>
+	</div>
+</div>
+</div>
                 </div>
             </div>
             <div id="mainBar">
@@ -47,26 +77,11 @@ $user = $_SESSION['CurrentUser'];
                     </div>
                 </div>
                 <div id="mainBarContent">
-                    <p>
-                        � 16. De Quincey, the essayist, once said that the German sentence is like a carryall - always room for one more. That used to be true of the English sentence. Originally, to be sure, our sentence was short, but under the influence of Latin studies it grew heavy and unwieldy. From sixteenth century writers it is possible to quote sentences of five or six hundred words. Such a sentence would fill two pages of this book.
 
-    When newspapers came to the front, the English sentence began to drop a part of its words. Yet one of the best journalists of the eighteenth century, Daniel Defoe, who wrote Robinson Crusoe, is not above writing an occasional sentence of great length. Here is a business sentence from Defoe:
-
-    One office for lone of money for customs of goods, which by a plain method might be so ordered that the merchant might with ease pay the highest customs down, and so, by allowing the bank four per cent advance, be first to secure the �10 per cent which the king allows for prompt payment at the custom house, and be also freed from the troublesome work of finding bondsmen and securities for the money - which has exposed many a man to the tyranny of extents, either for himself or his friend, to his utter ruin, who under a more moderate prosecution had been able to pay all his debts, and by this method has been torn to pieces and disabled from making any tolerable proposal to his creditors.
-
-    Here are a hundred and twenty-nine words in one sentence. The book from which it is taken, "An Essay upon Projects," averages more than sixty words to the sentence. How long is the average sentence today! It depends on the man, but in even the most literary prose it will not average more than thirty words. The average sentence of Macaulay's England is 23.43. Emerson's average sentence is less than that.
-                </p>
                 </div>
             </div>
-                    <div id="chatBar">
-                        <div id="titleBar">
-                            <div id="titleText">
-                                <h3>Chat</h3>
-                            </div>
-                        </div>
-                        <iframe src="http://localhost/TuPro/Home.php" height="700" width="230">
-                        </iframe>
-                    </div>
+
+
           </div>
       </div>
     <script src="js/jquery.min.js"></script>
