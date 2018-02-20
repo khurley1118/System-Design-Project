70fd27b114897430f564aaa3216ead1a0a8e5069
MarkEPat <markedward93@hotmail.com>
2018-02-20 12:13:08 -0400

    Remerging

--- Home.php
+++ Home.php
@@ -158,13 +158,10 @@
                      </div>
                   </div>
          </div><br>
-
        <!-- End Left Column -->
        </div>
-
        <!-- Right Column -->
        <div class="w3-twothird">
-
          <div class="w3-container w3-card w3-white w3-margin-bottom">
            <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i><?php echo $courseName; ?></h2>
            <?php
@@ -203,7 +200,5 @@
      </div>
      <!-- End Page Container -->
    </div>
-
-
    </body>
 </html>
