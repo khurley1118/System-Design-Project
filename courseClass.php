93181452e187235ec416d1acaf03dd5f5f3689bd
Jed Palmater <jed@palmater.ca>
2018-02-21 11:38:08 -0400

    course class created, and nameByCode method created in it

--- courseClass.php
+++ courseClass.php
@@ -1,5 +1,34 @@
+<?php
 class Course{
 	private $courseCode;
 	private $description;
 	private $isActive;
+
+	//sets
+	function setCourseCode($cCode){
+		$this->courseCode = $cCode;
+	}
+	function setDescription($desc){
+		$this->description = $desc;
+	}
+	function setIsActive($coursid){
+		$this->courseCode = $courseid;
+	}
+
+	//gets
+	function getCourseCode(){
+		return $this->courseCode;
+	}
+	function getDescription(){
+		return $this->description;
+	}
+	function getIsActive(){
+		return $this->isActive;
+	}
+
+	function nameByCode($con, $courseCode){
+		$courseName = DLgetCourseName($con, $courseCode);
+	}
+
 }
+?>
