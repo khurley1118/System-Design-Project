99748326c681fb6a6a552fddc218f71f590f6a3a
Mark <markedward93@hotmail.com>
2018-04-02 16:10:04 -0300

    Updated JS

--- js/AdminPanel.js
+++ js/AdminPanel.js
@@ -1,17 +1,4 @@
-function setCourses() {
-    document.getElementById('coursesContent').setAttribute("class", "showDiv");
-    document.getElementById('accountsContent').setAttribute("class", "hideDiv");
-    document.getElementById('searchAccount').setAttribute("class", "hideDiv");
-    document.getElementById('addAccount').setAttribute("class", "hideDiv");
-    document.getElementById('changePassword').setAttribute("class", "hideDiv");
-}
-function setAccounts() {
-    document.getElementById('coursesContent').setAttribute("class", "hideDiv");
-    document.getElementById('accountsContent').setAttribute("class", "showDiv");
-    document.getElementById('removeCourse').setAttribute("class", "hideDiv");
-    document.getElementById('addCourse').setAttribute("class", "hideDiv");
-    document.getElementById('showCourses').setAttribute("class", "hideDiv");
-}
+
 function addCourse() {
     document.getElementById('removeCourse').setAttribute("class", "hideDiv");
     document.getElementById('addCourse').setAttribute("class", "showDiv");
